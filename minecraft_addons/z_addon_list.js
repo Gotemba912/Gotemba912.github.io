d9f827baf76d0218355a383714b0091e662c30ef
優香 <78265558+Gotemba912@users.noreply.github.com>
2025-12-17 20:54:26 +0900

    かいしゅう

---
+++ minecraft_addons/z_addon_list.js
@@ -0,0 +1,212 @@
+let adlist = {
+  addon_list: [
+    {
+      name: "Newline Text for Anvil",
+      description: "金床で改行テキストを使用できます。",
+      type: ["リソースパック"],
+      junban: 1,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/Newline-Text_for_Anvil/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/Newline-Text_for_Anvil/releases/latest"
+    },
+    {
+      name: "Custom UI Maker",
+      description: "本と羽根ペンを使って内容を自由に入力して表示できるUIを作ることができます!",
+      type: ["リソースパック"],
+      junban: 4,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/custom-ui-maker/refs/heads/main/pack_icon.png",
+      link: "./custom_ui_maker.html"
+    },
+    {
+      name: "Legacy GameConsoleUI",
+      description: "コンソールエディション(WiiU, 旧Switch, PS3, 旧PS4, Xbox360, 旧Xbox One)のようなUIにするリソースパックです!",
+      type: ["リソースパック"],
+      junban: 5,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/ConsoleUI/refs/heads/main/pack_icon.png",
+      link: "./console_ui.html"
+    },
+    {
+      name: "Custom boss bar UI",
+      description: "カスタムボスバーをscoreboardのsidebarで作れます。<br>/scoreboard objectives add &lt;ID&gt; dummy",
+      type: ["リソースパック"],
+      junban: 6,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/Custom-bossbar-addon/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/Custom-bossbar-addon/releases/latest"
+    },
+    {
+      name: "チャットでセクション装飾消す",
+      description: "チャットでのセクション装飾を消します<br>全てのサーバーで動くわけではありません!",
+      type: ["リソースパック"],
+      junban: 7,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/anti_section_decoration_in_chat/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/anti_section_decoration_in_chat/releases/latest"
+    },
+    {
+      name: "Preview デバッグテキスト切り替え",
+      description: "Preview版の上のデバッグテキストを非表示にしたりできます。<br>HUD画面でF10を押して切り替えます。",
+      type: ["リソースパック"],
+      junban: 8,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/debug_text_toggle/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/debug_text_toggle/releases/latest"
+    },
+    {
+      name: "グリッド表示のサーバーフォーム",
+      description: "サーバーフォームをグリッド表示にします",
+      type: ["リソースパック"],
+      junban: 9,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/grid-sever-form/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/grid-sever-form/releases/latest"
+    },
+    {
+      name: "Chat Text Copy",
+      description: "チャットの文章を簡単にコピペできます",
+      type: ["リソースパック"],
+      junban: 10,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/chat-text-copy/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/chat-text-copy/releases/latest"
+    },
+    {
+      name: "Fullscreen Play Screen",
+      description: "Gote-Supporterにあったフルスクリーンの遊ぶ画面です。",
+      type: ["リソースパック"],
+      junban: 11,
+      icon_url: "../textures/minecraft_icon.png",
+      link: "https://github.com/Gotemba912/fullscreen-play-screen/releases/latest"
+    },
+    {
+      name: "SNS Feed Screen",
+      description: "Gote-SupporterにあったSNSみたいなフィード画面です。",
+      type: ["リソースパック"],
+      junban: 12,
+      icon_url: "../textures/minecraft_icon.png",
+      link: "https://github.com/Gotemba912/Compact_Feed_Screen/releases/latest"
+    },
+    {
+      name: "Fullscreen Bock Screen",
+      description: "Gote-Supporterにあったフルスクリーンの本画面です。",
+      type: ["リソースパック"],
+      junban: 13,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/fullscreen-bock-screen/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/fullscreen-bock-screen/releases/latest"
+    },
+    {
+      name: "タッチ操作でかんたんF5",
+      description: "F5キーをスマホで押せる！<br>camera コマンドの実験を有効化する必要があります。",
+      type: ["ビヘイビアーパック", "リソースパック"],
+      junban: 14,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/touch-f5/refs/heads/main/resource/pack_icon.png",
+      link: "https://github.com/Gotemba912/touch-f5/releases/latest"
+    },
+    {
+      name: "オンスクリーン効果",
+      description: "画面効果を追加します！",
+      type: ["リソースパック"],
+      junban: 16,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/screen-effect/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/screen-effect/releases/latest"
+    },
+    {
+      name: "Online Player Count",
+      description: "オンラインのプレイヤー数をポーズ画面に表示します！",
+      type: ["リソースパック"],
+      junban: 19,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/Online-Player-Count/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/Online-Player-Count/releases/latest"
+    },
+    {
+      name: "Tab Key Playerlist",
+      description: "このマイクラ統合版用リソースパックは、<br>HUDでTabキーを押すと、プレイヤーリストを表示することができます。",
+      type: ["リソースパック"],
+      junban: 20,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/Tabkey_Playerlist_UI/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/Tabkey_Playerlist_UI/releases/latest"
+    },
+    {
+      name: "水の音を昔のようにする for v1.20.30",
+      description: "水のサウンドを1.20.10以前のようにします 1.20.30にのみ対応",
+      type: ["リソースパック"],
+      junban: 22,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/fixed-water/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/fixed-water/releases/latest"
+    },
+    {
+      name: "Safezone filler",
+      description: "より小さいGUIにできます！",
+      type: ["リソースパック"],
+      junban: 23,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/safezone-filler/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/safezone-filler/releases/latest"
+    },
+    {
+      name: "三人称カメラの仕様を戻す",
+      description: "三人称カメラで音をプレイヤー基準の仕様に戻します",
+      type: ["ビヘイビアーパック"],
+      junban: 24,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/camera-sound-player/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/camera-sound-player/releases/latest"
+    },
+    {
+      name: "UIパフォーマンス改善",
+      description: "UIでのメモリ使用量を抑え、パフォーマンスを改善します",
+      type: ["リソースパック"],
+      junban: 26,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/memori-sakugen-ui/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/memori-sakugen-ui/releases/latest"
+    },
+    {
+      name: "Item ID Auxを見れるやつ",
+      description: "アイテムのItem ID Auxをインベントリから見れます。<br>タッチ非対応です",
+      type: ["リソースパック"],
+      junban: 28,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/item-id-aux-hyouzi/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/item-id-aux-hyouzi/releases/latest"
+    },
+    {
+      name: "Unicodeフォント化",
+      description: "フォントをUnicodeフォント化します<br>言語 日本語がおすすめです!",
+      type: ["リソースパック"],
+      junban: 29,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/compulsion-unicode-font/refs/heads/main/pack_icon.png",
+      link: "./compulsion-unicode-font.html"
+    },
+    {
+      name: "日本語フォントを小さくするリソースパック +",
+      description: "大きくなった日本語フォントを小さくする！ +文字化け改善！<br>グロリソ禁止ワールドでも機能します！",
+      type: ["リソースパック"],
+      junban: 30,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/small-font/refs/heads/main/pack_icon.png",
+      link: "./small-font.html"
+    },
+    {
+      name: "MCPE Alpha StartScreen",
+      description: "アルファ時代のスタート画面を再現しました。",
+      type: ["リソースパック"],
+      junban: 31,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/alpha_startscreen/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/alpha_startscreen/releases/latest"
+    },
+    {
+      name: "即抜けRTA",
+      description: "即抜けを検知して通知します!",
+      type: ["ビヘイビアーパック"],
+      junban: 32,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/Maikura_de_Sokunuke_RTA/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/Maikura_de_Sokunuke_RTA/releases/latest"
+    },
+    {
+      name: "OreUI風 ポップアップ",
+      description: "ポップアップダイアログをOreUI風にします",
+      type: ["リソースパック"],
+      junban: 33,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/OreUI-Popup/refs/heads/main/pack_icon.png",
+      link: "https://github.com/Gotemba912/OreUI-Popup/releases/latest"
+    },
+    {
+      name: "HUDチャット アニメーション",
+      description: "HUDのチャットにアニメーションを追加します！",
+      type: ["リソースパック"],
+      junban: 34,
+      icon_url: "https://raw.githubusercontent.com/Gotemba912/BetterChatAnimation/refs/heads/main/pack_icon.png",
+      link: "./chat_animation.html"
+    },
+  ]
+}
